64a2acd81abbe0df36dd6f9f1bd9482292c76529
Cole Fortner <colefortner@gmail.com>
2022-01-04 22:21:34 -0500

    Create Timer component to hold timer and add basic styling and title structure

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Platform } from "react-native";
 import { Focus } from "./src/features/focus/Focus";
+import { Timer } from "./src/features/timer/Timer";
 import { colors } from "./src/utils/colors";
+import { spacing } from "./src/utils/sizes";
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusSubject, setFocusSubject] = useState("gardening");
   return (
     <View style={styles.container}>
       {focusSubject ? (
-        <Text>I have subject</Text>
+        <Timer focusSubject={focusSubject} />
       ) : (
         <Focus addSubject={setFocusSubject} />
       )}
@@ -20,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === "ios" ? spacing.xxl : spacing.lg,
     backgroundColor: colors.darkBlue
-    // color: colors.white
   }
 });
