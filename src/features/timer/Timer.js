9ff1a5374863f50f77255f6a32928add73b29715
Cole Fortner <colefortner@gmail.com>
2022-01-06 17:52:42 -0500

    Add logic to make the timer count down and add some styling

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -8,7 +8,9 @@ import { spacing } from "../../utils/sizes";
 export const Timer = ({ focusSubject }) => {
   return (
     <View style={styles.container}>
-      <Countdown />
+      <View style={styles.countdown}>
+        <Countdown />
+      </View>
       <View style={{ paddingTop: spacing.xxl }}>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
@@ -29,5 +31,10 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontWeight: "bold",
     textAlign: "center"
+  },
+  countdown: {
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "center"
   }
 });
